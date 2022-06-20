81e28349cde71eff704843bc5921439cbf365b56
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-06-20 12:32:30 +0200

    Added a function that ensures that the currently entered letter and the previously entered letter are displayed while the other letters are still covered.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ welcome_message = f'H A N G M A N # {attempts} attempts'
 words_to_find = ['python', 'java', 'swift', 'javascript']
 word_for_game = random.choice(words_to_find)
 word_letters_as_set = set(word_for_game)
+letters_already_found = []
 
 word_all_covered = ''.join(['-' for letter in word_for_game])
 
@@ -14,13 +15,14 @@ print(word_letters_as_set)  # for debug purposes only: show the letters in the w
 print(welcome_message)
 print(word_all_covered)
 
-while attempts != 0:
-    strings_for_output = []
+while attempts == 8:
     user_input = input('Input a letter: ')
     print(attempts)  # for debug purposes only: show number of remaining attempts
 
     if user_input in word_letters_as_set:
         output_string = ''.join([letter if letter == user_input else '-' for letter in word_for_game])
+        letters_already_found.append(user_input)
+        print(letters_already_found)  # for debug purposes only: show letters already found
         print(output_string)
 
     else:
@@ -28,4 +30,20 @@ while attempts != 0:
 
     attempts -= 1
 
+while attempts != 0:
+    print(f'{attempts}')  # for debug purposes only: show number of remaining attempts
 
+    print(''.join([letter for letter in word_for_game if letter == letters_already_found]))
+    user_input = input('Input a letter: ')
+
+    if user_input in word_letters_as_set and user_input not in letters_already_found:
+        output_string = [letter if letter == user_input or letter in letters_already_found
+                         else '-' for letter in word_for_game]
+        letters_already_found.append(user_input)
+        print(letters_already_found)  # for debug purposes only: show letters already found
+        print(''.join(output_string))
+
+    else:
+        print('NO')
+
+    attempts -= 1
