95f05cdc752e8a536788db19ccaadf1627838ccb
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-06-20 12:50:38 +0200

    Program is fully functional. However, the formatting of the output does not yet match the specifications.

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ letters_already_found = []
 
 word_all_covered = ''.join(['-' for letter in word_for_game])
 
-print(word_for_game)  # for debug purposes only: show the relevant word
-print(word_letters_as_set)  # for debug purposes only: show the letters in the word without duplicates.
+# print(word_for_game)  # for debug purposes only: show the relevant word
+# print(word_letters_as_set)  # for debug purposes only: show the letters in the word without duplicates.
 
 print(welcome_message)
 print(word_all_covered)
@@ -26,12 +26,13 @@ while attempts == 8:
         print(output_string)
 
     else:
-        print('NO')
+        print(word_all_covered)
+        print("That letter doesn't appear in the word")
 
     attempts -= 1
 
 while attempts != 0:
-    print(f'{attempts}')  # for debug purposes only: show number of remaining attempts
+    # print(f'{attempts}')  # for debug purposes only: show number of remaining attempts
 
     print(''.join([letter for letter in word_for_game if letter == letters_already_found]))
     user_input = input('Input a letter: ')
@@ -40,10 +41,19 @@ while attempts != 0:
         output_string = [letter if letter == user_input or letter in letters_already_found
                          else '-' for letter in word_for_game]
         letters_already_found.append(user_input)
-        print(letters_already_found)  # for debug purposes only: show letters already found
+        # print(letters_already_found)  # for debug purposes only: show letters already found
         print(''.join(output_string))
 
+    elif user_input in letters_already_found:
+        # print('continue')  # for debug purposes only
+        continue
+
     else:
-        print('NO')
+        output_string = [letter if letter == letter in letters_already_found
+                         else '-' for letter in word_for_game]
+        print(''.join(output_string))
+        print("That letter doesn't appear in the word")
 
     attempts -= 1
+
+print('Thanks for playing!')
